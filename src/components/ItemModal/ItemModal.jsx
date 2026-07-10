cd5c50983b89c7897ac46228a92d9237d6f0b6bb
Bjorn Peineke <bjpeineke@gmail.com>
2026-07-10 12:40:19 -0400

    New update project 11

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,18 +1,17 @@
 import "./ItemModal.css";
 
-function ItemModal({ activeModal,OnClose, Card }) {
-    return (
-        <div className="modal">
-            <div className={`modal__content modal__content_type_image ${activeModal === "preview" ? "modal__content_open" : ""}`}   >
-                <button 
-                    onClick={OnClose}
-                    type="button" 
-                    className="modal__close"
-                >
-                    CLOSE
-                </button>
-            </div>
-    );
+function ItemModal({ activeModal, OnClose, Card }) {
+  return (
+    <div className="modal">
+      <div
+        className={`modal__content modal__content_type_image ${activeModal === "preview" ? "modal__content_open" : ""}`}
+      >
+        <button onClick={OnClose} type="button" className="modal__close">
+          CLOSE
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default ItemModal;
